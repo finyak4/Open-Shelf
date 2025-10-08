901d5f975dcec61a81b198c80850ba7e08951259
vlad <vlad.finyak4201@gmail.com>
2025-10-08 16:23:03 +0200

    Entrypoint for Propuction

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,9 +1,23 @@
 #!/bin/sh
 set -e
 
+# For local development
+# wait_for_db() {
+#   echo "Waiting for database to become available..."
+#   while ! pg_isready -h "$POSTGRES_HOST" -p "$POSTGRES_PORT" -U "$POSTGRES_USER"; do
+#     sleep 1
+#   done
+#   echo "Database is available!"
+# }  
+
+# For production (e.g., Render.com)
+
 wait_for_db() {
   echo "Waiting for database to become available..."
-  while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
+  DB_HOST=$(echo "$DATABASE_URL" | sed -E 's/^.*@([^:/]+):[0-9]+\/.*$/\1/')
+  DB_PORT=$(echo "$DATABASE_URL" | sed -E 's/^.*:([0-9]+)\/.*$/\1/')
+
+  while ! pg_isready -h "$DB_HOST" -p "$DB_PORT" > /dev/null 2>&1; do
     sleep 1
   done
   echo "Database is available!"
